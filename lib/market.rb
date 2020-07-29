0af52e6087caa53e4ca5565f51d5c11dde4bd66f
Hope Gochnour <hope.gochnour@gmail.com>
2020-07-29 10:34:42 -0600

    Update Market to be able to determine vendors' names

--- lib/market.rb
+++ lib/market.rb
@@ -12,4 +12,12 @@ class Market
   def add_vendor(vendor)
     @vendors << vendor
   end
+
+  def vendor_names
+    names = []
+    @vendors.each do |vendor|
+      names << vendor.name
+    end
+    return names
+  end
 end
--- test/market_test.rb
+++ test/market_test.rb
@@ -31,4 +31,36 @@ class MarketTest < Minitest::Test
 
     assert_equal [vendor1], market.vendors
   end
+
+  def test_the_vendors_have_names
+    market = Market.new("South Pearl Street Farmers Market")
+    vendor1 = Vendor.new("Rocky Mountain Fresh")
+    vendor2 = Vendor.new("Ba-Nom-a-Nom")
+    vendor3 = Vendor.new("Palisade Peach Shack")
+    market.add_vendor(vendor1)
+    market.add_vendor(vendor2)
+    market.add_vendor(vendor3)
+
+    assert_equal ["Rocky Mountain Fresh", "Ba-Nom-a-Nom", "Palisade Peach Shack"], market.vendor_names
+  end
+
+  def test_its_vendors_can_hold_items
+    market = Market.new("South Pearl Street Farmers Market")
+    vendor1 = Vendor.new("Rocky Mountain Fresh")
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    item2 = Item.new({name: 'Tomato', price: "$0.50"})
+    item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+    vendor2 = Vendor.new("Ba-Nom-a-Nom")
+    vendor3 = Vendor.new("Palisade Peach Shack")
+    vendor1.stock(item1, 35)
+    vendor1.stock(item2, 7)
+    vendor2.stock(item4, 50)
+    vendor2.stock(item3, 25)
+    vendor3.stock(item1, 65)
+    market.add_vendor(vendor1)
+    market.add_vendor(vendor2)
+    market.add_vendor(vendor3)
+
+  end
 end
